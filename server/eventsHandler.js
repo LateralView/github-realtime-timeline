d99f59c1d08b75ee9db6c5a66c07f6edb6b71068
Diego García <diego@lateralview.net>
2016-02-22 18:05:55 -0300

    Add plain text message

--- server/eventsHandler.js
+++ server/eventsHandler.js
@@ -11,7 +11,8 @@ function getEventInfo(req) {
         var commit_id = req.body.comment.commit_id.substring(0,10);
         var url = req.body.comment.html_url;
         var message = sender + " commented on commit [" + repo + "@" + commit_id + "](" + url + ")";
-        info = { message: message, extra: req.body.comment.body, avatar: req.body.sender.avatar_url, date: date };
+        var plain_text = sender + " commented on commit " + repo + "@" + commit_id;
+        info = { message: message, extra: req.body.comment.body, avatar: req.body.sender.avatar_url, date: date, plain_text: plain_text };
         break;
 
     case "push":
@@ -19,14 +20,15 @@ function getEventInfo(req) {
         var branch = req.body.ref.match(/([^\/]*)\/*$/)[1];
         var branch_url = url + "/tree/" + branch;
         var message = sender + " pushed to [" + branch + "](" + branch_url + ") at [" + repo + "](" + url + ")";
+        var plain_text = sender + " pushed to " + branch + " at " + repo;
 
         var commits = req.body.commits;
         if (commits.length) {
           var commit_text = (commits.length == 1)? "commit" : "commits";
           var extra = "[" + commits.length + " " + commit_text + "](" + req.body.compare + ")";
-          info = { message: message, extra: extra, avatar: req.body.sender.avatar_url, date: date };
+          info = { message: message, extra: extra, avatar: req.body.sender.avatar_url, date: date, plain_text: plain_text };
         } else
-          info = { message: message, avatar: req.body.sender.avatar_url, date: date };
+          info = { message: message, avatar: req.body.sender.avatar_url, date: date, plain_text: plain_text };
 
         break;
 
@@ -40,7 +42,8 @@ function getEventInfo(req) {
             var branch = req.body.ref;
             var branch_url = url + "/tree/" + branch;
             var message = sender + " created branch [" + branch + "](" + branch_url + ") at [" + repo + "](" + url + ")";
-            info = { message: message, avatar: req.body.sender.avatar_url, date: date };
+            var plain_text = sender + " created branch " + branch + " at " + repo;
+            info = { message: message, avatar: req.body.sender.avatar_url, date: date, plain_text: plain_text };
             break;
         }
 
@@ -49,7 +52,8 @@ function getEventInfo(req) {
     case "repository":
         var url = req.body.repository.html_url;
         var message = sender + " created repository [" + repo + "](" + url + ")";
-        info = { message: message, avatar: req.body.sender.avatar_url, date: date };
+        var plain_text = sender + " created repository " + repo;
+        info = { message: message, avatar: req.body.sender.avatar_url, date: date, plain_text: plain_text };
         break;
 
     case "pull_request":
@@ -58,17 +62,20 @@ function getEventInfo(req) {
         var number = req.body.pull_request.number;
         var extra = req.body.pull_request.title;
         var message = "";
+        var plain_text = "";
 
         switch(type) {
 
           case "opened":
             message = sender + " opened pull request [" + repo + "#" + number + "](" + url + ")";
-            info = { message: message, avatar: req.body.sender.avatar_url, extra: extra, date: date };
+            plain_text = sender + " opened pull request " + repo + "#" + number;
+            info = { message: message, avatar: req.body.sender.avatar_url, extra: extra, date: date, plain_text: plain_text };
             break;
 
           case "closed":
             message = sender + " merged pull request [" + repo + "#" + number + "](" + url + ")";
-            info = { message: message, avatar: req.body.sender.avatar_url, extra: extra, date: date };
+            plain_text = sender + " merged pull request " + repo + "#" + number;
+            info = { message: message, avatar: req.body.sender.avatar_url, extra: extra, date: date, plain_text: plain_text };
             break;
         }
 
@@ -80,22 +87,26 @@ function getEventInfo(req) {
         var number = req.body.issue.number;
         var extra = req.body.issue.title;
         var message = "";
+        var plain_text = "";
 
         switch(type) {
 
           case "opened":
             message = sender + " opened issue [" + repo + "#" + number + "](" + url + ")";
-            info = { message: message, avatar: req.body.sender.avatar_url, extra: extra, date: date };
+            plain_text = sender + " opened issue " + repo + "#" + number;
+            info = { message: message, avatar: req.body.sender.avatar_url, extra: extra, date: date, plain_text: plain_text };
             break;
 
           case "closed":
             message = sender + " closed issue [" + repo + "#" + number + "](" + url + ")";
-            info = { message: message, avatar: req.body.sender.avatar_url, extra: extra, date: date };
+            plain_text = sender + " closed issue " + repo + "#" + number;
+            info = { message: message, avatar: req.body.sender.avatar_url, extra: extra, date: date, plain_text: plain_text };
             break;
 
           case "reopened":
             message = sender + " reopened issue [" + repo + "#" + number + "](" + url + ")";
-            info = { message: message, avatar: req.body.sender.avatar_url, extra: extra, date: date };
+            plain_text = sender + " reopened issue " + repo + "#" + number;
+            info = { message: message, avatar: req.body.sender.avatar_url, extra: extra, date: date, plain_text: plain_text };
             break;
         }
 
@@ -106,7 +117,8 @@ function getEventInfo(req) {
         var number = req.body.issue.number;
         var extra = req.body.comment.body;
         var message = sender + " commented on issue [" + repo + "#" + number + "](" + url + ")";
-        info = { message: message, extra: req.body.comment.body, avatar: req.body.sender.avatar_url, date: date };
+        var plain_text = sender + " commented on issue " + repo + "#" + number;
+        info = { message: message, extra: req.body.comment.body, avatar: req.body.sender.avatar_url, date: date, plain_text: plain_text };
         break;
 
     case "pull_request_review_comment":
@@ -114,7 +126,8 @@ function getEventInfo(req) {
         var number = req.body.pull_request.number;
         var extra = req.body.comment.body;
         var message = sender + " commented on pull request [" + repo + "#" + number + "](" + url + ")";
-        info = { message: message, extra: req.body.comment.body, avatar: req.body.sender.avatar_url, date: date };
+        var plain_text = sender + " commented on pull request " + repo + "#" + number;
+        info = { message: message, extra: req.body.comment.body, avatar: req.body.sender.avatar_url, date: date, plain_text: plain_text };
         break;
 
   }
